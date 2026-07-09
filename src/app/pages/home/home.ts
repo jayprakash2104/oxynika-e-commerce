2ecc350bb49ba94e42ac75a48b0c20d3e6b28d3c
jayprakash satpathy <jayprakashsatpathy.99@gmail.com>
2026-07-09 15:54:12 +0530

    why-choose section done

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -4,11 +4,12 @@ import { Navbar } from "../../sections/navbar/navbar";
 import { Hero } from "../../sections/hero/hero";
 import { Highlights } from "../../sections/highlights/highlights";
 import { Featured } from "../../sections/featured/featured";
+import { WhyChoose } from "../../sections/why-choose/why-choose";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [Header, Navbar, Hero, Highlights, Featured],
+  imports: [Header, Navbar, Hero, Highlights, Featured, WhyChoose],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
---
+++ src/app/sections/why-choose/why-choose.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-why-choose',
+  standalone: true,
+  imports: [],
+  templateUrl: './why-choose.html',
+  styleUrl: './why-choose.scss',
+})
+export class WhyChoose {
+
+}
